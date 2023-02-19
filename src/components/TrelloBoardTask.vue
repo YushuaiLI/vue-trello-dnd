dac5601f1278590d9355303ad6ed82d83e184930
Daniele Tortora <tortora87@gmail.com>
2023-02-19 14:05:42 +0100

    fix input style

--- src/components/TrelloBoardTask.vue
+++ src/components/TrelloBoardTask.vue
@@ -9,7 +9,7 @@ defineProps<{
 <template>
   <div
     :title="task.createdAt.toLocaleDateString()"
-    class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px]"
+    class="bg-white p-2 mb-2 rounded shadow-sm max-w-[250px]"
   >
     <span>{{ task.title }}</span>
   </div>
